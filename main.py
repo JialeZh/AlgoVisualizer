520fc333468658f8e6cf81d00b3e5b2d0e6ba25c
JialeZh <jiale13799@gmail.com>
2021-07-09 15:44:58 -0500

    Create base Tkinter GUI

---
+++ main.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter import ttk
+import random
+
+WHITE = '#FFFFFF'
+
+window = Tk()
+window.title("Algorithms Visualizer")
+window.maxsize(1200, 900)
+window.config(bg = WHITE)
+
+data = []
+
+algo_name = StringVar()
+algo_list = ['Bubble Sort']
+
+
+
+window.mainloop()
