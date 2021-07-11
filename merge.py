2e97b128c51363a013b0b43d2d3f61567c033db4
JialeZh <jiale13799@gmail.com>
2021-07-11 16:18:09 -0500

    Implement merge sort

--- merge.py
+++ merge.py
@@ -1,1 +1,39 @@
+import time
 
+def mergeSort(data, start, mid, end, drawData, timeTick):
+    a = start
+    b = mid + 1
+    tempArray = []
+
+    for i in range(start, end+1):
+        if a > mid:
+            tempArray.append(data[b])
+            q+=1
+        elif b > end:
+            tempArray.append(data[a])
+            a+=1
+        elif data[p] < data[q]:
+            tempArray.append(data[a])
+            a+=1
+        else:
+            tempArray.append(data[b])
+            b+=1
+
+    for a in range(len(tempArray)):
+        data[start] = tempArray[a]
+        start += 1
+
+def merge(data, start, end, drawData, timeTick):
+    if start < end:
+        mid = int((start + end) / 2)
+        merge(data, start, mid, drawData, timeTick)
+        merge(data, mid+1, end, drawData, timeTick)
+
+        mergeSort(data, start, mid, end, drawData, timeTick)
+
+        drawData(data, ['#BF01FB' if x >= start and x < mid else '#F7E806' if x == mid 
+                        else '#4204CC' if x > mid and x <=end else '#0CA8F6' for x in range(len(data))])
+        time.sleep(timeTick)
+
+    drawData(data, ['#0CA8F6' for x in range(len(data))])
+    
